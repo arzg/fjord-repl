c1e59d612acc50903c78525a5d71d89520496e5a
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-08-19 19:41:34 +1000

    Refactor processing of chunk of input into function

--- src/bin/fjord.rs
+++ src/bin/fjord.rs
@@ -15,24 +15,30 @@ fn main() -> io::Result<()> {
         let mut input = String::new();
         stdin.read_line(&mut input)?;
 
-        let parser = Parser::new(&input);
-        let parse_output = parser.parse();
-
-        println!("{}", parse_output.debug_tree());
-
-        if let Some(parse_output) = parse_output.clone().into_no_errors() {
-            println!("No errors were found. Evaluating...");
-
-            let eval_result = parse_output.eval(&mut env);
-            dbg!(&eval_result);
-        } else {
-            println!("Errors:");
-
-            for error in parse_output.errors() {
-                dbg!(error);
-            }
-
-            println!("Syntax errors were found. Skipping evaluation.");
-        }
+        process_chunk(&input, &mut env, &mut stdout)?;
     }
+}
+
+fn process_chunk(text: &str, env: &mut Env, stdout: &mut io::Stdout) -> io::Result<()> {
+    let parser = Parser::new(text);
+    let parse_output = parser.parse();
+
+    println!("{}", parse_output.debug_tree());
+
+    if let Some(parse_output) = parse_output.clone().into_no_errors() {
+        writeln!(stdout, "No errors were found. Evaluating...")?;
+
+        let eval_result = parse_output.eval(env);
+        dbg!(&eval_result);
+    } else {
+        writeln!(stdout, "Errors:")?;
+
+        for error in parse_output.errors() {
+            dbg!(error);
+        }
+
+        writeln!(stdout, "Syntax errors were found. Skipping evaluation.")?;
+    }
+
+    Ok(())
 }
